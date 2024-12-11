fb63e1a29f972162b309b3ce0dc387f5fd73ba1e
Marco <ma8155354@gmail.com>
2024-12-10 22:53:38 -0300

    New animations at the projects page

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,51 +1,66 @@
+const menu = document.getElementById("menu");
+const menuItems = document.getElementById("items-home-menu");
 
-const menu = document.getElementById('menu')
-const menuItems = document.getElementById('items-home-menu')
+menu.addEventListener("click", () => {
+  if (menuItems.style.display === "none") {
+    menu.style.zIndex = "2";
+    menuItems.style.display = "flex";
+  } else {
+    menuItems.style.display = "none";
+  }
 
-menu.addEventListener('click', () => {
-
-    if (menuItems.style.display === "none") {
-        menu.style.zIndex = "2"
-        menuItems.style.display = "flex"
-    }
-    else {
-        menuItems.style.display = "none"
-    }
-
-})
+  if (window.innerWidth >= 1000 && menuItems.style.display === "none") {
+    menuItems.style.display = "flex";
+  }
+});
 
 function SwitchThemes() {
-    if (document.body.classList.contains('light-theme')) {
-        document.body.classList.remove('light-theme')
-        document.body.classList.add('dark-theme')
-    }
-    else{
-        document.body.classList.add('light-theme')
-        document.body.classList.remove('dark-theme')
-    }
+  if (document.body.classList.contains("light-theme")) {
+    document.body.classList.remove("light-theme");
+    document.body.classList.add("dark-theme");
+  } else {
+    document.body.classList.add("light-theme");
+    document.body.classList.remove("dark-theme");
+  }
 }
 
+// Intro Animation
+const intro = document.querySelector(".intro");
+const logoHeader = document.querySelector(".logo-header");
+const logo = document.querySelector(".logo");
 
-const intro = document.querySelector('.intro')
-const logoHeader = document.querySelector('.logo-header')
-const logo = document.querySelector('.logo')
+document.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    setTimeout(() => {
+      logo.classList.add("fadeIn");
+    }, 600);
 
-document.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+      logo.classList.remove("fadeIn");
+      logo.classList.add("fadeOut");
+    }, 2000);
 
-    setTimeout(() =>{
+    setTimeout(() => {
+      intro.style.top = "-100vh";
+    }, 2300);
+  });
+});
 
-        setTimeout(() => {
-            logo.classList.add('fadeIn')
-        }, 400)
+window.addEventListener("resize", () => {
+  if (window.innerWidth >= 1000 && menuItems.style.display === "none") {
+    menuItems.style.display = "flex";
+  }
+});
 
-        setTimeout(() => {
-            logo.classList.remove('fadeIn')
-            logo.classList.add('fadeOut')
-        }, 2000)
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
 
-        setTimeout(() => {
-            intro.style.top = "-100vh"
-        },2300)
-    })
-
-})
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((element) => observer.observe(element));
